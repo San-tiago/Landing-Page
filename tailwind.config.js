acd425fd79b840fae96e2e0c99c27ce931858ad0
San-tiago <62841279+San-tiago@users.noreply.github.com>
2021-07-12 15:38:46 +0800

    done

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  colors: {
+     primary:'#1D1D1F',
+   
+    },
+    fontFamily: {
+    
+      pop: ['Poppins'],
+    },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
